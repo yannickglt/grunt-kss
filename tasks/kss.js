88896f5a741c4efe44ff2200bcfe2b57fefd5bc8
t32k <ijok.ijok@gmail.com>
2014-12-08 01:09:10 -0800

    Reformat all files

--- tasks/kss.js
+++ tasks/kss.js
@@ -8,97 +8,94 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.registerMultiTask('kss', 'Generate styleguide by KSS.', function() {
+  grunt.registerMultiTask('kss', 'Generate styleguide by KSS.', function () {
 
-        var fs = require('fs'),
-            path = require('path'),
-            exec = require('child_process').exec,
-            done = this.async();
+    var fs = require('fs'),
+      path = require('path'),
+      exec = require('child_process').exec,
+      done = this.async();
 
-        var kssCmd = ['node'],
-            realPath = path.dirname(__filename).replace(/tasks$/g, ''),
-            sass = require(realPath + '/lib/sass'),
-            dest = process.cwd();
+    var kssCmd = ['node'],
+      realPath = path.dirname(__filename).replace(/tasks$/g, ''),
+      sass = require(realPath + '/lib/sass'),
+      dest = process.cwd();
 
-        var opts = this.options({
-            template: null,
-            includeType: null,
-            includePath: null,
-            loadPath: null,
-            helpers: null,
-            config: null,
-            mask: null,
-            scssRoot: [],
-            outputStyle: 'compressed'
-        });
-        
-        // Allow sub tasks to override includePath
-        if (this.data.includePath) {
-            opts.includePath = this.data.includePath;
-        }
-
-
-        kssCmd.push(realPath + 'node_modules/kss/bin/kss-node');
-
-        this.files.forEach(function(file) {
-            kssCmd.push(file.src[0]);
-            kssCmd.push(file.dest);
-            dest = file.dest;
-        });
-
-        if (opts.template !== null) {
-            kssCmd.push('--template', opts.template);
-        }
-
-        if (opts.mask !== null) {
-            kssCmd.push('--mask', opts.mask);
-        }
-
-        if (opts.includeType !== null && opts.includePath !== null) {
-            kssCmd.push('--' + opts.includeType, opts.includePath);
-        }
-        if (opts.loadPath!== null) {
-            kssCmd.push('--load-path', opts.loadPath);
-        }
-        if (opts.helpers!== null) {
-            kssCmd.push('--helpers', opts.helpers);
-        }
-        if (opts.config!== null) {
-            kssCmd.push('--config', opts.config);
-        }
-
-        var putInfo = function(error, result, code) {
-            if (error !== null) {
-                grunt.log.error(error);
-                grunt.log.error('Code: ' + code);
-            } else {
-                grunt.log.write(result);
-            }
-
-            // Compile scss file if include type is "scss".
-            if (opts.includeType === 'scss' && path.extname(opts.includePath) === '.scss') {
-                opts.scssRoot = opts.scssRoot.map(function(root) {
-                    return path.resolve(process.cwd() + '/' + root);
-                });
-
-                grunt.log.writeln('Scss includes compilation.');
-                grunt.log.writeln(opts.includePath + ' -> ' + dest.replace(/\/$/, '') + '/public/style.css');
-
-                grunt.file.write(
-                    dest.replace(/\/$/, '') + '/public/style.css',
-                    sass.compile(opts.includePath, opts.scssRoot, opts.outputStyle),
-                    {encoding: 'utf8'}
-                );
-            }
-            done();
-        };
-
-        // Execute!!
-      console.log(kssCmd.join(' '));
-        exec(kssCmd.join(' '), putInfo);
-        grunt.verbose.ok('`[KSS] ' + kssCmd.join(' ') + '` was initiated.');
-
+    var opts = this.options({
+      template: null,
+      includeType: null,
+      includePath: null,
+      loadPath: null,
+      helpers: null,
+      config: null,
+      mask: null,
+      scssRoot: [],
+      outputStyle: 'compressed'
     });
+
+    // Allow sub tasks to override includePath
+    if (this.data.includePath) {
+      opts.includePath = this.data.includePath;
+    }
+
+    kssCmd.push(realPath + 'node_modules/kss/bin/kss-node');
+
+    this.files.forEach(function (file) {
+      kssCmd.push(file.src[0]);
+      kssCmd.push(file.dest);
+      dest = file.dest;
+    });
+
+    if (opts.template !== null) {
+      kssCmd.push('--template', opts.template);
+    }
+    if (opts.mask !== null) {
+      kssCmd.push('--mask', opts.mask);
+    }
+    if (opts.includeType !== null && opts.includePath !== null) {
+      kssCmd.push('--' + opts.includeType, opts.includePath);
+    }
+    if (opts.loadPath !== null) {
+      kssCmd.push('--load-path', opts.loadPath);
+    }
+    if (opts.helpers !== null) {
+      kssCmd.push('--helpers', opts.helpers);
+    }
+    if (opts.config !== null) {
+      kssCmd.push('--config', opts.config);
+    }
+
+    var putInfo = function (error, result, code) {
+      if (error !== null) {
+        grunt.log.error(error);
+        grunt.log.error('Code: ' + code);
+      } else {
+        grunt.log.write(result);
+      }
+
+      // Compile scss file if include type is "scss".
+      if (opts.includeType === 'scss' && path.extname(opts.includePath) === '.scss') {
+        opts.scssRoot = opts.scssRoot.map(function (root) {
+          return path.resolve(process.cwd() + '/' + root);
+        });
+
+        grunt.log.writeln('Scss includes compilation.');
+        grunt.log.writeln(opts.includePath + ' -> ' + dest.replace(/\/$/, '') + '/public/style.css');
+
+        grunt.file.write(
+          dest.replace(/\/$/, '') + '/public/style.css',
+          sass.compile(opts.includePath, opts.scssRoot, opts.outputStyle),
+          {encoding: 'utf8'}
+        );
+      }
+      done();
+    };
+
+    // Execute
+    console.log(kssCmd.join(' '));
+    exec(kssCmd.join(' '), putInfo);
+    grunt.verbose.ok('`[KSS] ' + kssCmd.join(' ') + '` was initiated.');
+
+  });
 };
